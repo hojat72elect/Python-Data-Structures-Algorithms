a17f9b3ab22f4a26ed6d7d8fc766553f0a264f9d
hojat72elect <hojat72elect@gmail.com>
2025-11-03 00:03:20 -0500

    Added generic types to Stack.

--- src/Stack.py
+++ src/Stack.py
@@ -1,30 +1,39 @@
-class Stack:
-    def __init__(self):
-        self.dataHolder = []
+from typing import TypeVar, Generic
 
-    def push(self, new_value):
+T = TypeVar('T')
+
+class Stack(Generic[T]):
+    def __init__(self):
+        self.dataHolder: list[T] = []
+
+    def push(self, new_value: T):
         """Pushes an item on top of the stack."""
         self.dataHolder.append(new_value)
 
-    def isEmpty(self):
+    def isEmpty(self) -> bool:
         return len(self.dataHolder) == 0
 
-    def pop(self):
+    def pop(self) -> T:
         """Removes and returns the element on top of the stack"""
         if self.isEmpty():
             raise IndexError("Cannot pop an empty stack")
-
         return self.dataHolder.pop()
 
-    def peek(self):
+    def peek(self) -> T | None:
         """Returns the top element without removing it from the stack."""
         if self.isEmpty():
-            raise IndexError("Cannot peek into an empty stack")
-
+            return None
         return self.dataHolder[-1]
 
-    def size(self):
+    def getSize(self):
         return len(self.dataHolder)
 
     def __str__(self):
-        return str(self.dataHolder)
+        if self.isEmpty():
+            return "Stack is empty"
+        representation = "--- Top ---\n"
+        for item in reversed(self.dataHolder):
+            representation += f"| {item}\n"
+        representation += "--- Bottom ---"
+
+        return representation
