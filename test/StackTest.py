6c6ef4b6d84a0053d415e8ca68ce8c955bcc0714
hojat72elect <hojat72elect@gmail.com>
2025-11-03 00:09:54 -0500

    Fixed the failing tests of Stack.

--- test/StackTest.py
+++ test/StackTest.py
@@ -8,9 +8,9 @@ class StackTest(unittest.TestCase):
 
     def test_push(self):
         self.stack.push(1)
-        self.assertEqual(self.stack.size(), 1)
+        self.assertEqual(self.stack.getSize(), 1)
         self.stack.push(2)
-        self.assertEqual(self.stack.size(), 2)
+        self.assertEqual(self.stack.getSize(), 2)
         self.assertEqual(self.stack.peek(), 2)
 
     def test_is_empty(self):
@@ -22,9 +22,9 @@ class StackTest(unittest.TestCase):
         self.stack.push(1)
         self.stack.push(2)
         self.assertEqual(self.stack.pop(), 2)
-        self.assertEqual(self.stack.size(), 1)
+        self.assertEqual(self.stack.getSize(), 1)
         self.assertEqual(self.stack.pop(), 1)
-        self.assertEqual(self.stack.size(), 0)
+        self.assertEqual(self.stack.getSize(), 0)
         self.assertTrue(self.stack.isEmpty())
 
     def test_pop_empty_stack(self):
@@ -36,26 +36,25 @@ class StackTest(unittest.TestCase):
         self.assertEqual(self.stack.peek(), 1)
         self.stack.push(2)
         self.assertEqual(self.stack.peek(), 2)
-        self.assertEqual(self.stack.size(), 2)
+        self.assertEqual(self.stack.getSize(), 2)
 
     def test_peek_empty_stack(self):
-        with self.assertRaises(IndexError):
-            self.stack.peek()
+        self.assertIsNone(self.stack.peek())
 
     def test_size(self):
-        self.assertEqual(self.stack.size(), 0)
+        self.assertEqual(self.stack.getSize(), 0)
         self.stack.push(1)
-        self.assertEqual(self.stack.size(), 1)
+        self.assertEqual(self.stack.getSize(), 1)
         self.stack.push(2)
-        self.assertEqual(self.stack.size(), 2)
+        self.assertEqual(self.stack.getSize(), 2)
         self.stack.pop()
-        self.assertEqual(self.stack.size(), 1)
+        self.assertEqual(self.stack.getSize(), 1)
         self.stack.pop()
-        self.assertEqual(self.stack.size(), 0)
+        self.assertEqual(self.stack.getSize(), 0)
 
     def test_str(self):
-        self.assertEqual(str(self.stack), "[]")
+        self.assertEqual(str(self.stack), "Stack is empty")
         self.stack.push(1)
-        self.assertEqual(str(self.stack), "[1]")
+        self.assertEqual(str(self.stack), "--- Top ---\n| 1\n--- Bottom ---")
         self.stack.push(2)
-        self.assertEqual(str(self.stack), "[1, 2]")
+        self.assertEqual(str(self.stack), "--- Top ---\n| 2\n| 1\n--- Bottom ---")
