015d94bcb955a4445655f707c7bc9bcb4656b132
hojat72elect <hojat72elect@gmail.com>
2025-11-03 04:49:09 -0500

    Renamed.

--- test/StackTest.py
+++ test/StackTest.py
@@ -4,57 +4,57 @@ from src.Stack import Stack
 class StackTest(unittest.TestCase):
 
     def setUp(self):
-        self.stack = Stack()
+        self.sut = Stack()
 
     def test_push(self):
-        self.stack.push(1)
-        self.assertEqual(self.stack.getSize(), 1)
-        self.stack.push(2)
-        self.assertEqual(self.stack.getSize(), 2)
-        self.assertEqual(self.stack.peek(), 2)
+        self.sut.push(1)
+        self.assertEqual(self.sut.getSize(), 1)
+        self.sut.push(2)
+        self.assertEqual(self.sut.getSize(), 2)
+        self.assertEqual(self.sut.peek(), 2)
 
     def test_is_empty(self):
-        self.assertTrue(self.stack.isEmpty())
-        self.stack.push(1)
-        self.assertFalse(self.stack.isEmpty())
+        self.assertTrue(self.sut.isEmpty())
+        self.sut.push(1)
+        self.assertFalse(self.sut.isEmpty())
 
     def test_pop(self):
-        self.stack.push(1)
-        self.stack.push(2)
-        self.assertEqual(self.stack.pop(), 2)
-        self.assertEqual(self.stack.getSize(), 1)
-        self.assertEqual(self.stack.pop(), 1)
-        self.assertEqual(self.stack.getSize(), 0)
-        self.assertTrue(self.stack.isEmpty())
+        self.sut.push(1)
+        self.sut.push(2)
+        self.assertEqual(self.sut.pop(), 2)
+        self.assertEqual(self.sut.getSize(), 1)
+        self.assertEqual(self.sut.pop(), 1)
+        self.assertEqual(self.sut.getSize(), 0)
+        self.assertTrue(self.sut.isEmpty())
 
     def test_pop_empty_stack(self):
         with self.assertRaises(IndexError):
-            self.stack.pop()
+            self.sut.pop()
 
     def test_peek(self):
-        self.stack.push(1)
-        self.assertEqual(self.stack.peek(), 1)
-        self.stack.push(2)
-        self.assertEqual(self.stack.peek(), 2)
-        self.assertEqual(self.stack.getSize(), 2)
+        self.sut.push(1)
+        self.assertEqual(self.sut.peek(), 1)
+        self.sut.push(2)
+        self.assertEqual(self.sut.peek(), 2)
+        self.assertEqual(self.sut.getSize(), 2)
 
     def test_peek_empty_stack(self):
-        self.assertIsNone(self.stack.peek())
+        self.assertIsNone(self.sut.peek())
 
     def test_size(self):
-        self.assertEqual(self.stack.getSize(), 0)
-        self.stack.push(1)
-        self.assertEqual(self.stack.getSize(), 1)
-        self.stack.push(2)
-        self.assertEqual(self.stack.getSize(), 2)
-        self.stack.pop()
-        self.assertEqual(self.stack.getSize(), 1)
-        self.stack.pop()
-        self.assertEqual(self.stack.getSize(), 0)
+        self.assertEqual(self.sut.getSize(), 0)
+        self.sut.push(1)
+        self.assertEqual(self.sut.getSize(), 1)
+        self.sut.push(2)
+        self.assertEqual(self.sut.getSize(), 2)
+        self.sut.pop()
+        self.assertEqual(self.sut.getSize(), 1)
+        self.sut.pop()
+        self.assertEqual(self.sut.getSize(), 0)
 
     def test_str(self):
-        self.assertEqual(str(self.stack), "Stack is empty")
-        self.stack.push(1)
-        self.assertEqual(str(self.stack), "--- Top ---\n| 1\n--- Bottom ---")
-        self.stack.push(2)
-        self.assertEqual(str(self.stack), "--- Top ---\n| 2\n| 1\n--- Bottom ---")
+        self.assertEqual(str(self.sut), "Stack is empty")
+        self.sut.push(1)
+        self.assertEqual(str(self.sut), "--- Top ---\n| 1\n--- Bottom ---")
+        self.sut.push(2)
+        self.assertEqual(str(self.sut), "--- Top ---\n| 2\n| 1\n--- Bottom ---")
