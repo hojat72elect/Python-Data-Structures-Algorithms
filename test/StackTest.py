2dbf54c726c59b97b9698a85c3d42062833dbee2
hojat72elect <hojat72elect@gmail.com>
2025-11-02 22:53:44 -0500

    Added tests for stack.

---
+++ test/StackTest.py
@@ -0,0 +1,61 @@
+import unittest
+from src.Stack import Stack
+
+class StackTest(unittest.TestCase):
+
+    def setUp(self):
+        self.stack = Stack()
+
+    def test_push(self):
+        self.stack.push(1)
+        self.assertEqual(self.stack.size(), 1)
+        self.stack.push(2)
+        self.assertEqual(self.stack.size(), 2)
+        self.assertEqual(self.stack.peek(), 2)
+
+    def test_is_empty(self):
+        self.assertTrue(self.stack.isEmpty())
+        self.stack.push(1)
+        self.assertFalse(self.stack.isEmpty())
+
+    def test_pop(self):
+        self.stack.push(1)
+        self.stack.push(2)
+        self.assertEqual(self.stack.pop(), 2)
+        self.assertEqual(self.stack.size(), 1)
+        self.assertEqual(self.stack.pop(), 1)
+        self.assertEqual(self.stack.size(), 0)
+        self.assertTrue(self.stack.isEmpty())
+
+    def test_pop_empty_stack(self):
+        with self.assertRaises(IndexError):
+            self.stack.pop()
+
+    def test_peek(self):
+        self.stack.push(1)
+        self.assertEqual(self.stack.peek(), 1)
+        self.stack.push(2)
+        self.assertEqual(self.stack.peek(), 2)
+        self.assertEqual(self.stack.size(), 2)
+
+    def test_peek_empty_stack(self):
+        with self.assertRaises(IndexError):
+            self.stack.peek()
+
+    def test_size(self):
+        self.assertEqual(self.stack.size(), 0)
+        self.stack.push(1)
+        self.assertEqual(self.stack.size(), 1)
+        self.stack.push(2)
+        self.assertEqual(self.stack.size(), 2)
+        self.stack.pop()
+        self.assertEqual(self.stack.size(), 1)
+        self.stack.pop()
+        self.assertEqual(self.stack.size(), 0)
+
+    def test_str(self):
+        self.assertEqual(str(self.stack), "[]")
+        self.stack.push(1)
+        self.assertEqual(str(self.stack), "[1]")
+        self.stack.push(2)
+        self.assertEqual(str(self.stack), "[1, 2]")
